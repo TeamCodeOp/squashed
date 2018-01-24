1a34ed2e8dddebd134a115f6cb434bb82e8661fb
Tejaswi <tejaswi.reddy6@gmail.com>
2018-01-24 15:25:39 -0800

    removed packages from package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
-/**
- * These rules enforce the airbnb Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/airbnb/javascript
- */
 module.exports = {
   "extends": "airbnb",
   "rules": {
-    "func-names": ["error", "never"]
-  }
+    "func-names": ["error", "never"],
+    "react/prefer-stateless-function": [<enabled>, { "ignorePureComponents": <ignorePureComponents> }],
+  },
 }
