884526a2d6a5cd5d5b7723a2afca015985b7e4db
Tejaswi Guvvala <30578313+tguvvala@users.noreply.github.com>
2018-01-29 11:19:22 -0800

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  CLIENT_ID: 'a7e80a0632c4dbd30144',
-  CLIENT_SECRET: 'acfde3cb6b5e9a91c031e85751aebe505d4e1604',
-  DB_USERNAME: 'b4563830505e5c',
-  DB_PASSWORD: '2590904e',
-  DB_NAME: 'heroku_e13a74604d192c8',
-  DB_HOST: 'us-cdbr-iron-east-05.cleardb.net'
-};
-
