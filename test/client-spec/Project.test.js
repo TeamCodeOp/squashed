9baf91b706a759dedd57b20c80e294f956bcc62d
Ralph Plumley <ralphplumley@gmail.com>
2018-01-31 22:29:56 -0500

    Minor client test updates

---
+++ test/client-spec/Project.test.js
@@ -0,0 +1,25 @@
+import { JSDOM } from 'jsdom';
+
+import React from 'react';
+import { expect } from 'chai';
+import { mount, shallow } from 'enzyme';
+import { spy } from 'sinon';
+
+import Project from '../../react-client/src/components/Project.jsx';
+
+describe('<Project />', () => {
+
+  const props = {
+    match: {
+      params: ''
+    }
+  };
+
+  it('renders without exploding', () => {
+    expect(
+      shallow(
+        <Project {...props} />
+      ).length
+    ).to.equal(1);
+  });
+});
