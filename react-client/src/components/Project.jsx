046fe51b224e2e3376d18dfdce9f68404264f549
toowan <mail@toowan.me>
2018-02-01 16:36:12 -0800

    Add ReactDisqusThread

--- react-client/src/components/Project.jsx
+++ react-client/src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import ReactDisqusThread from 'react-disqus-thread';
 import { Grid, Image, Item } from 'semantic-ui-react';
 
 class Project extends React.Component {
