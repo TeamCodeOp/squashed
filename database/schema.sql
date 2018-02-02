f3c770333fedad3508bc335b022e3ab055bc44be
Daniel Kelly <djk8552@gmail.com>
2018-02-02 14:15:36 -0500

    Create table if not exist technologies

--- database/schema.sql
+++ database/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS projects (
 
 -- mysql --host=DB_HOST --user=DB_USER--password=DB_PASS --reconnect DB_NAME < schema.sql
 
-CREATE TABLE technologies (
+CREATE TABLE IF NOT EXISTS technologies (
   id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   tech_name varchar(20) NOT NULL,
   project_id int,
