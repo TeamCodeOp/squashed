ff80e2bfd810633d9a68ad7f53388fd810a0a050
Tejaswi <tejaswi.reddy6@gmail.com>
2018-02-06 17:07:30 -0800

    resolved conflicts

--- server/index.js
+++ server/index.js
@@ -212,6 +212,7 @@ app.post('/projects', (req, res) => {
 
 
 
+
 app.post('/getCurrentUserProfileId', (req, res) => {
   console.log('get request /getCurrentUserProfileId in (server / index.js)');
 
@@ -266,6 +267,7 @@ app.put('/projects', (req, res) => {
   console.log('here in projects', req.body);
 
 });
+
 
 
 // delete request to the projects schema
