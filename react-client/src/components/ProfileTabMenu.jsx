22c4b49c6a99272dadc6d53c6799464551325cf2
Daniel Kelly <djk8552@gmail.com>
2018-02-12 23:10:18 -0500

    Clean up ProfileTabMenu

--- react-client/src/components/ProfileTabMenu.jsx
+++ react-client/src/components/ProfileTabMenu.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Menu, Segment, Grid } from 'semantic-ui-react';
 import UserProjectList from './UserProjectList.jsx';
-import MessageList from './MessageList.jsx'
+import MessageList from './MessageList.jsx';
 
 export default class ProfileTabMenu extends Component {
   constructor(props) {
     super(props);
-    this.state = { activeItem: '' };
+    this.state = { activeItem: 'Projects' };
 
     this.handleItemClick = this.handleItemClick.bind(this);
   }
@@ -20,28 +20,27 @@ export default class ProfileTabMenu extends Component {
     return (
       <div>
         <Menu pointing secondary>
-          <Menu.Item name='Projects' active={activeItem === 'Projects'} onClick={this.handleItemClick} />
-          <Menu.Item name='Messages' active={activeItem === 'Messages'} onClick={this.handleItemClick} />
+          <Menu.Item name="Projects" active={activeItem === 'Projects'} onClick={this.handleItemClick} />
+          <Menu.Item name="Inbox" active={activeItem === 'Inbox'} onClick={this.handleItemClick} />
         </Menu>
 
         <Segment>
           {this.state.activeItem === 'Projects' &&
             <Grid>
-                {this.props.projects.map((project, i) => {
-                  if (i % 3 === 0 && i <= this.props.projects.length - 1) {
-                    return <UserProjectList key={i} items={this.props.projects.slice(i, i + 3)} />;
-                  }
-                })}
-              </Grid>
-
+              {this.props.projects.map((project, i) => {
+                if (i % 3 === 0 && i <= this.props.projects.length - 1) {
+                  return <UserProjectList key={i} items={this.props.projects.slice(i, i + 3)} />;
+                }
+              })}
+            </Grid>
           }
 
-          {this.state.activeItem === 'Messages' && (this.props.id && (this.props.id === this.props.currentProfileId)) &&
+          {this.state.activeItem === 'Inbox' && (this.props.id && (this.props.id === this.props.currentProfileId)) &&
           <MessageList
-          messages={this.props.messages}
-          handleDeleteMessage={this.props.handleDeleteMessage}
-        />
-        }
+            messages={this.props.messages}
+            handleDeleteMessage={this.props.handleDeleteMessage}
+          />
+          }
         </Segment>
       </div>
     );
